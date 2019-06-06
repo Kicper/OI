70a90c58d4b860c5569d63ea8185c7daf80f92c2
Kicper <51452942+Kicper@users.noreply.github.com>
2019-06-06 19:08:30 +0200

    Create main.cpp

---
+++ Olimpiady/15/Etap 1/Plakatowanie/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int arr[250002], pointer=1, posters=0, n, temp;
+    arr[0]=0;
+
+    ifstream infile;
+    ofstream outfile;
+    infile.open("pla10b.in", ios::in);
+    outfile.open("result.out", ios::out);
+    infile>>n;
+    for(int i=0; i<n; ++i) {
+        infile>>temp;
+        infile>>temp;
+        if(arr[pointer-1] < temp) {
+            posters++;
+            arr[pointer]=temp;
+            pointer++;
+        }
+        else {
+            while(arr[pointer-1]>temp) {
+                arr[pointer-1]=0;
+                pointer--;
+            }
+            if(arr[pointer-1]<temp) {
+                arr[pointer]=temp;
+                pointer++;
+                posters++;
+            }
+        }
+    }
+    outfile<<posters;
+    infile.close();
+    outfile.close();
+
+    return 0;
+}
