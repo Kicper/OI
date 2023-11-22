0ee209af8219d6eb8a8bc4b5864a0048dc5eca8f
Kacper Kipa <51452942+Kicper@users.noreply.github.com>
2023-11-22 20:23:53 +0100

    Update main.cpp

--- Olimpiady/30/Etap 1/Płytkie nawiasowania/main.cpp
+++ Olimpiady/30/Etap 1/Płytkie nawiasowania/main.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <stdio.h>
 
+using namespace std;
+
+int main()
+{
+    int n, H, h = 0, result = 0;
+    char arr[1000000];
+    cin >> n >> H;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+        if (arr[i] == '(') {
+            h++;
+            if (h > H) {
+                result++;
+                h -= 2;
+            }
+        }
+        else if (arr[i] == ')') {
+            h--;
+            if (h < 0) {
+                result++;
+                h += 2;
+            }
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    cout << result;
+    return 0;
+}
